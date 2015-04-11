e6525e4a61121560a326dc03d793f5d038eaa066
skyrudy <ruddra90@gmail.com>
2015-04-12 03:17:12 +0600

    +--

---
+++ core/static/js/overrides/profile.js
@@ -0,0 +1,33 @@
+function updateAbtMe() {
+   $('.update_abt_me').hide();
+   $('.abt_me_text').hide();
+   $('.abt_me_form').show();
+};
+
+function changeMajor() {
+   $('.change_major').hide();
+   $('.change_major_text').hide();
+   $('.change_major_form').show();
+};
+
+$(document).ready(function() {
+    $('form').submit(function() {
+    $(this).hide();
+        $.ajax({
+            data: $(this).serialize(),
+            type: "post",
+            url:  $(this).attr('action'),
+            success: function(response) {
+                var c = $(response.update_data_class);
+                var b = $(response.update_btn_class);
+//                c.replaceWith('<p> '+ response.value +' </p>')
+                c.text(response.value);
+                c.show();
+                b.show();
+
+
+            }
+        });
+        return false;
+    });
+});
